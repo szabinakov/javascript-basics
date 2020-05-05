7229cddebd7169fae76f347a38177b5a82b9b805
Szabina Kovacs <szabinkov@gmail.com>
2020-05-05 19:59:37 +0100

    objects

--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,59 @@
 const createPerson = (name, age) => {
-  // your code here
+  const myObject = {
+    name,
+    age
+  };
+  return myObject;
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object.age;
 };
 
 const hasProperty = (property, object) => {
-  // your code here
-};
+  return object.hasOwnProperty(property);
+}; 
 
 const isOver65 = person => {
-  // your code here
+  if (person.age > 65) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const getAges = people => {
-  // your code here
+  /*
+  people.map(x => {
+    return x.age;
+  });*/
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find(object => name === object.name );
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter(object => object.manufacturer === "Honda" )
 };
 
 const averageAge = people => {
-  // your code here
+  const theAge = people.map(person => person.age);
+
+  function getAvg(theAge) {
+    const total = theAge.reduce((acc, c) => acc + c, 0);
+    return total / theAge.length;
+  }
+  const theAvr = getAvg(theAge);
+  return theAvr;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  return object
 };
 
 module.exports = {
